b5469b43b0bd97e86bbbd70fbbc49543c14ca442
Jairo <101882230+JairoPA@users.noreply.github.com>
2024-12-17 14:32:52 -0600

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,32 +1,35 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-// Configuración de Nodemailer
+// Función para habilitar CORS
+const enableCORS = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite solicitudes de cualquier dominio
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+};
+
+// Configuración del transporter de Nodemailer
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com', // Host del servidor SMTP de Gmail
-  port: 465, // Puerto para conexiones seguras (SSL)
-  secure: true, // true para usar SSL, false para usar TLS o STARTTLS
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Usar SSL
   auth: {
-    user: 'preciadojairo82@gmail.com', // Tu correo
-    pass: 'fgfv fcnt cwqr rwva', // Contraseña de la aplicación
+    user: 'preciadojairo82@gmail.com',
+    pass: 'fgfv fcnt cwqr rwva', // Asegúrate de usar la contraseña de aplicación correcta
   },
 });
 
 export default async function handler(req, res) {
-  // Habilitar CORS para solicitudes desde cualquier origen (si es necesario)
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde cualquier origen
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST'); // Métodos permitidos
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Cabeceras permitidas
-
-  // Si la solicitud es de tipo OPTIONS (usualmente para CORS pre-flight), responde con 200 OK
+  // Manejo de opciones (pre-flight request)
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    enableCORS(req, res);
+    return res.status(200).end();
   }
 
   if (req.method === 'POST') {
     const { email, code } = req.body;
 
     try {
+      // Enviar el correo
       await transporter.sendMail({
         from: '"Lightbulb Support" <preciadojairo82@gmail.com>',
         to: email,
@@ -40,7 +43,7 @@ export default async function handler(req, res) {
       res.status(500).json({ error: 'Error al enviar el correo' });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader('Allow', ['POST', 'OPTIONS']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
