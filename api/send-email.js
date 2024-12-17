61cd9dc9b65366c8b359364ecfae9fa753619413
Jairo <101882230+JairoPA@users.noreply.github.com>
2024-12-17 14:16:02 -0600

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -2,16 +2,27 @@ const nodemailer = require('nodemailer');
 
 // Configuración de Nodemailer
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', // Host del servidor SMTP de Gmail
-    port: 465, // Puerto para conexiones seguras (SSL)
-    secure: true, // true para usar SSL, false para usar TLS o STARTTLS
-    auth: {
-      user: 'preciadojairo82@gmail.com', // Tu correo
-      pass: 'fgfv fcnt cwqr rwva', // Contraseña de la aplicación
-    },
-  });
+  host: 'smtp.gmail.com', // Host del servidor SMTP de Gmail
+  port: 465, // Puerto para conexiones seguras (SSL)
+  secure: true, // true para usar SSL, false para usar TLS o STARTTLS
+  auth: {
+    user: 'preciadojairo82@gmail.com', // Tu correo
+    pass: 'fgfv fcnt cwqr rwva', // Contraseña de la aplicación
+  },
+});
 
 export default async function handler(req, res) {
+  // Habilitar CORS para solicitudes desde cualquier origen (si es necesario)
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde cualquier origen
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST'); // Métodos permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Cabeceras permitidas
+
+  // Si la solicitud es de tipo OPTIONS (usualmente para CORS pre-flight), responde con 200 OK
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'POST') {
     const { email, code } = req.body;
 
